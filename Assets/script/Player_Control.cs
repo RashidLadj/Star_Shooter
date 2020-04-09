38eba96d05e74e0c561abad7309e44b59379a467
LADJOUZI rachid <59792800+RashidLadj@users.noreply.github.com>
2020-04-09 16:24:59 +0200

    RotationPlayer

--- Assets/script/Player_Control.cs
+++ Assets/script/Player_Control.cs
@@ -10,6 +10,7 @@ public class Boundary{
 public class Player_Control : MonoBehaviour {
 	public float vitesse;
 	public Boundary boundary;
+	public float rotationvVaisseau;	
 	// Use this for initialization
 	void Start () {
 		
@@ -17,15 +18,18 @@ public class Player_Control : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/** Deplacement **/
 		float deplacementHorizontal = Input.GetAxis("Vertical");
 		float deplacementVertical = Input.GetAxis("Horizontal");
 		Vector3 mouvement = new Vector3(-deplacementHorizontal, 0, deplacementVertical);
 		GetComponent<Rigidbody>().velocity = mouvement * vitesse;	
+		/** limit Scene ***/
 		GetComponent<Rigidbody>().position = new Vector3(
 			Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
 			GetComponent<Rigidbody>().position.y,
 			Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 		);
-
+		/** Rotation **/
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler(0f, 0f, GetComponent<Rigidbody>().velocity.x * -rotationvVaisseau);
 	}	
 }
